0366318d277047cda18c38a62d69f2af1e3a5c84
Leo Asadourian <33045765+Leoasa1@users.noreply.github.com>
2023-05-08 15:59:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 const express = require('express');
 const dataBase = require('./config/db');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
 require('dotenv').config();
 
-const isProduction = process.env.NODE_ENV === 'production';
-const origin = isProduction
-	? 'https://movie-night-ten.vercel.app/'
-	: 'http://localhost:3000';
 const port = process.env.PORT || 5000;
 
 dataBase();
 
-app.use(cors({ credentials: true, origin }));
 app.use(cookieParser());
 app.use(express.json());
 app.use('/movies', require('./routes/movies'));
 app.use('/watchlist', require('./routes/watchlist'));
 
 app.get('/', (req, res) => {
-	res.send(`Server Running! Is in Production: ${isProduction}`);
+	res.send(`Server Running!`);
 });
 
-app.listen(port, () => {
-	console.log(
-		`Server listening at http://localhost:${port} in ${
-			isProduction ? 'production' : 'development'
-		} mode`
-	);
-});
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
